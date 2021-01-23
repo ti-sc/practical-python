8050c8064879bd8b0d76f4f65639892d886a7893
ti-sc <till.schmidt.55@gmail.com>
2021-01-23 14:24:47 +0100

    1.31 not working

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    total = 0
+    with open(filename,'rt') as portfolio:
+        next(portfolio)
+        for line in portfolio:
+            row = line.split(',')
+            try:
+                n = int(row[1])
+                m = float(row[2])
+                total = total + n * m
+            except ValueError:
+                print('Data is corrupted',line)
+        
+    return(total)
+
+cost = portfolio_cost('Work/Data/portfolio.csv')
+print('Total cost: ',cost)

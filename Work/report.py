2c859e1b77830ba0d7341a78d9d0c95b345d99f7
ti-sc <till.schmidt.55@gmail.com>
2021-02-17 20:04:01 +0100

    Excercise 2.6

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,44 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+
+def portfolio_cost(filename):
+    portfolio = []
+    
+    with open(filename,'rt') as file:    
+        rows = csv.reader(file)
+        headers = next(rows)
+        for row in rows:
+            holding = {
+                        headers[0] : row[0] ,
+                        headers[1] : int(row[1]) ,
+                        headers[2] : float(row[2]) 
+                      } 
+            portfolio.append(holding)
+    return(portfolio)
+
+
+def read_prices(filename):
+    import csv
+    prices = []
+    with open('Data/prices.csv', 'rt') as file:
+        rows = csv.reader(file)
+        try:
+            for row in rows:
+                print(row)
+                try:
+                    holding = {
+                            'name' : row[0] ,
+                            'price': float(row[1])
+                          }
+                    prices.append(holding)
+                except ValueError:
+                    
+                    print('ValueProblem')
+        except IndexError:
+             print('indexError')
+    return(prices)
+        
